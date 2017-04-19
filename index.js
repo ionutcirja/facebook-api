a13f5a3c6ed1d922b9bfab8062940f32f5ae0744
Ionut Cirja <ionut.cirja@gmail.com>
2017-04-19 15:48:05 +0100

    npm publish setup

--- index.js
+++
@@ -1,9 +0,0 @@
-import init from './src/init';
-import login from './src/login';
-import share from './src/share';
-
-export default {
-    init,
-    login,
-    share,
-};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import init from './init';
+import login from './login';
+import share from './share';
+
+export default {
+    init,
+    login,
+    share,
+};
